a12a4f8ccfe28cdda2c6d3024f400e0918fd35d3
Luis00809 <davidcarvajal008@gmail.com>
2024-02-19 13:13:11 -0800

    updates models

---
+++ StimuliApp/Migrations/20240219211120_ModelRevisions.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StimuliApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelRevisions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Stimuli",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SetId",
+                table: "Stimuli",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ClientId",
+                table: "StimSets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "StimSets",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Clients",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Clients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stimuli_SetId",
+                table: "Stimuli",
+                column: "SetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StimSets_ClientId",
+                table: "StimSets",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_UserId",
+                table: "Clients",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clients_Users_UserId",
+                table: "Clients",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StimSets_Clients_ClientId",
+                table: "StimSets",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stimuli_StimSets_SetId",
+                table: "Stimuli",
+                column: "SetId",
+                principalTable: "StimSets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clients_Users_UserId",
+                table: "Clients");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StimSets_Clients_ClientId",
+                table: "StimSets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stimuli_StimSets_SetId",
+                table: "Stimuli");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Stimuli_SetId",
+                table: "Stimuli");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StimSets_ClientId",
+                table: "StimSets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Clients_UserId",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Stimuli");
+
+            migrationBuilder.DropColumn(
+                name: "SetId",
+                table: "Stimuli");
+
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "StimSets");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "StimSets");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Clients");
+        }
+    }
+}
--- StimuliApp/obj/Debug/net8.0/StimuliApp.AssemblyInfo.cs
+++ StimuliApp/obj/Debug/net8.0/StimuliApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb5c93d858aa6dc128b059d88892feec6b9f1f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8482fbf35096a21b2c987ffeaf698f56a9e507c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
