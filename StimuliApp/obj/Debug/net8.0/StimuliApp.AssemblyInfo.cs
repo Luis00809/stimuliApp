de94ae2195983ab086a10155f1131c43245b5398
Luis00809 <davidcarvajal008@gmail.com>
2024-02-20 15:01:27 -0800

    j

--- StimuliApp/obj/Debug/net8.0/StimuliApp.AssemblyInfo.cs
+++ StimuliApp/obj/Debug/net8.0/StimuliApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29dbb110225fcf0ea7404d8ae4375bfb54b58370")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+191e5af7bcd081af7c056f83ba32a9b860779304")]
 [assembly: System.Reflection.AssemblyProductAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
