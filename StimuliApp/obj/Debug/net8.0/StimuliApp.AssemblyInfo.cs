2c61d0e6ac119ca2df53193a3abc7897daafb7c0
Luis00809 <davidcarvajal008@gmail.com>
2024-02-20 14:58:48 -0800

    sets up seeds

--- StimuliApp/obj/Debug/net8.0/StimuliApp.AssemblyInfo.cs
+++ StimuliApp/obj/Debug/net8.0/StimuliApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8482fbf35096a21b2c987ffeaf698f56a9e507c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29dbb110225fcf0ea7404d8ae4375bfb54b58370")]
 [assembly: System.Reflection.AssemblyProductAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
