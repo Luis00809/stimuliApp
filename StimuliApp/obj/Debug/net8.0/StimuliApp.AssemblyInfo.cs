0d377f7c4f7aebf567e35917fb597729d0249a6d
Luis00809 <davidcarvajal008@gmail.com>
2024-02-23 13:55:37 -0800

    ignore

--- StimuliApp/obj/Debug/net8.0/StimuliApp.AssemblyInfo.cs
+++ StimuliApp/obj/Debug/net8.0/StimuliApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de94ae2195983ab086a10155f1131c43245b5398")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebf90691dda1c613a50f38c7f3d955cafc07b1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
