258ae402016882004109acb0256272749c4d3c78
Luis00809 <davidcarvajal008@gmail.com>
2024-04-05 11:51:54 -0700

    deletes clients trials and rounds

---
+++ StimuliApp/Migrations/20240405182041_AddCascadeDeleteToTrials.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StimuliApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCascadeDeleteToTrials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trials_Clients_ClientId",
+                table: "Trials");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trials_Clients_ClientId",
+                table: "Trials",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rounds_Trials_TrialId",
+                table: "Rounds");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rounds_Trials_TrialId",
+                table: "Rounds",
+                column: "TrialId",
+                principalTable: "Trials",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- StimuliApp/obj/Debug/net8.0/StimuliApp.AssemblyInfo.cs
+++ StimuliApp/obj/Debug/net8.0/StimuliApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e4d15be80a0afc640ef9613268a664b03394c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa549607104c385af88f6a629681c440fa89eccf")]
 [assembly: System.Reflection.AssemblyProductAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StimuliApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
