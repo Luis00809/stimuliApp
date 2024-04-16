c7ae3b409e13f7535e99a1c349783a0cf2bd8f60
Luis00809 <davidcarvajal008@gmail.com>
2024-04-16 15:13:04 -0700

    fixes last set of errors"

--- StimuliApp/Models/payload.cs
+++ StimuliApp/Models/payload.cs
@@ -2,8 +2,8 @@ namespace StimuliApp.Models
 {
     public class AuthenticationResponse
     {
-        public string Token { get; set; }
+        public string? Token { get; set; }
         public int Id { get; set; }
-        public string Email { get; set; }
+        public string? Email { get; set; }
     }
 }
